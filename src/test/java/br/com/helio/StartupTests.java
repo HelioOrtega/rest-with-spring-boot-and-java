a761e3f2f7c137341cfe67711a8199907a94f300
Helio Ortega <helio.ortega.neto@gmail.com>
2024-04-04 15:46:22 -0300

    First Steps in Java with Spring Boot 3

---
+++ src/test/java/br/com/helio/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.helio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
