a860b63227b65ac9d10f8e89dba3dff2cc2d7089
Helio Ortega <helio.ortega.neto@gmail.com>
2024-04-05 20:54:16 -0300

    Inclusao de configuracao para aceitar Json e XML

---
+++ src/main/java/br/com/helio/config/WebConfig.java
@@ -0,0 +1,24 @@
+package br.com.helio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        WebMvcConfigurer.super.configureContentNegotiation(configurer);
+
+        //http://localhost:8080/api/person/v1?mediaType=xml
+
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+}
