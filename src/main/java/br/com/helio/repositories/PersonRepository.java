72cddeea570a8b1296c1bbc12372746fd3163262
Helio Ortega <helio.ortega.neto@gmail.com>
2024-04-06 14:19:30 -0300

    Implementacao de HATEOAS nos servicos da API

--- src/main/java/br/com/helio/repositories/PersonRepository.java
+++ src/main/java/br/com/helio/repositories/PersonRepository.java
@@ -1,7 +1,9 @@
 package br.com.helio.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.helio.model.Person;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {}
