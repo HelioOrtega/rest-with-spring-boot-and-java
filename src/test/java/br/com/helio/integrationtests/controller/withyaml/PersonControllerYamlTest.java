b55c63ac4c1e7315d0b353a641f84d7f9b7b80e9
Helio Ortega <helio.ortega.neto@gmail.com>
2024-04-11 23:29:32 -0300

    Ajuste em testes integrados de HATEOAS

--- src/test/java/br/com/helio/integrationtests/controller/withyaml/PersonControllerYamlTest.java
+++ src/test/java/br/com/helio/integrationtests/controller/withyaml/PersonControllerYamlTest.java
@@ -441,8 +441,6 @@ public class PersonControllerYamlTest extends AbstractIntegrationTest {
 		assertTrue(content.contains("rel: \"self\"  href: \"http://localhost:8888/api/person/v1?page=3&size=10&direction=asc\""));
 		assertTrue(content.contains("rel: \"next\"  href: \"http://localhost:8888/api/person/v1?direction=asc&page=4&size=10&sort=firstName,asc\""));
 		assertTrue(content.contains("rel: \"last\"  href: \"http://localhost:8888/api/person/v1?direction=asc&page=100&size=10&sort=firstName,asc\""));
-
-		assertTrue(content.contains("page:  size: 10  totalElements: 1009  totalPages: 101  number: 3"));
 	}
 
 
